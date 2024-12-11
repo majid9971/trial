00fab8779f409c32a2ff4818c3f676001c6cadc7
Majid Siddiqui <97821088+majid9971@users.noreply.github.com>
2024-12-11 11:29:39 +0530

    Create sample-script.sh

---
+++ scripts/sample-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Starting custom script execution..."
+
+# Display current date and time
+echo "Current Date and Time: $(date)"
+
+# List all files in the current directory
+echo "Listing all files in the current directory:"
+ls -l
+
+# Display the current working directory
+echo "Current Working Directory: $(pwd)"
+
+#Completion message
+echo "Custom script execution completed!"
